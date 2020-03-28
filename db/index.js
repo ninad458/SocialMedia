5d3cae1d763e5663aa597a7d1e74853b7ce0f621
Ninad Thakare <23650386+ninadthakare@users.noreply.github.com>
2020-03-28 12:12:20 +0530

    added mongoose library and set up connection

---
+++ db/index.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+module.exports = () => {
+    mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+    const db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'connection error:'));
+    db.once('open', function () {
+        console.log("Connected")
+    });
+}
